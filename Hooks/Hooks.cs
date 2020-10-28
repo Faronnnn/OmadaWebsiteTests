d9d853a4a53c363ee1f94a2fd1617eb152497c3d
Faronnnn <52966054+Faronnnn@users.noreply.github.com>
2020-10-28 21:41:17 +0100

    Final shortest working version of firefoxOption

--- Hooks/Hooks.cs
+++ Hooks/Hooks.cs
@@ -83,33 +83,17 @@ namespace OmadaWebsiteTests.Hooks
                     firefoxOptions.SetPreference("pdfjs.enabledCache.state", false); // new line that doesn't change shit
                     //firefoxOptions.SetPreference("pdfjs.disabled", true);
                     //var dr = new FirefoxDriver(firefoxProfile);*/
-                    CodePagesEncodingProvider.Instance.GetEncoding(437);
-                    Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+                    
+                    //working version:
+                    //CodePagesEncodingProvider.Instance.GetEncoding(437);
+                    //Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
                     //Directory.CreateDirectory("C:\\Utility\\Downloads");
 
-                    FirefoxProfile profile = new FirefoxProfile();
-                    profile.SetPreference("browser.download.dir", filesDowloadPath);
-                    profile.SetPreference("browser.download.folderList", 2);
-                    profile.SetPreference("browser.download.useDownloadDir", true);
-                    profile.SetPreference("browser.helperApps.neverAsk.saveToDisk", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;image/jpeg;application/pdf;application/octet-stream");
-                    //profile.SetPreference("browser.download.manager.showWhenStarting", false);
-                    profile.SetPreference("browser.helperApps.neverAsk.openFile", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;image/jpeg;application/pdf;application/octet-stream");
-                    profile.SetPreference("browser.helperApps.alwaysAsk.force", false);
-                    profile.SetPreference("browser.download.manager.useWindow", false);
-                    profile.SetPreference("browser.download.manager.focusWhenStarting", false);
-                    profile.SetPreference("browser.download.manager.showAlertOnComplete", false);
-                    profile.SetPreference("browser.download.manager.closeWhenDone", true);
-                    profile.SetPreference("pdfjs.enabledCache.state", false);
-                    profile.SetPreference("plugin.disable_full_page_plugin_for_types", "application/pdf");
-                    profile.SetPreference("pref.downloads.disable_button.edit_actions", false);
-                    profile.SetPreference("browser.download.manager.alertOnEXEOpen", false);
-                    profile.SetPreference("browser.download.manager.closeWhenDone", true);
-                    profile.SetPreference("browser.download.manager.showAlertOnComplete", false);
-                    profile.SetPreference("browser.download.manager.useWindow", false);
-                    profile.SetPreference("services.sync.prefs.sync.browser.download.manager.showWhenStarting", false);
-                    profile.SetPreference("pdfjs.disabled", true);
                     FirefoxOptions firefoxOptions = new FirefoxOptions();
-                    firefoxOptions.Profile = profile;
+                    firefoxOptions.SetPreference("browser.download.dir", filesDowloadPath);
+                    firefoxOptions.SetPreference("browser.download.folderList", 2);
+                    firefoxOptions.SetPreference("browser.helperApps.neverAsk.saveToDisk", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;image/jpeg;application/pdf;application/octet-stream");
+                    firefoxOptions.SetPreference("pdfjs.disabled", true);
                     return new FirefoxDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), firefoxOptions, TimeSpan.FromSeconds(15));
                 case string browser: 
                     throw new NotSupportedException($"{browser} is not a supported browser");
